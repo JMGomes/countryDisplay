1be8b6b9495a3b4a5974aa2d067fdb66573e369d
Joao Gomes <joaoo@spotify.com>
2022-08-23 17:42:54 +0100

    set up cypress tests

---
+++ cypress/e2e/countryList.cy.ts
@@ -0,0 +1,19 @@
+describe('country list', () => {
+  it('should navigate correctly', () => {
+    cy.visit('localhost:3000');
+
+    cy.contains('Afghanistan');
+
+    cy.get('a[href*="/country/AFG"]').click();
+
+    cy.url().should('include', '/country/AFG');
+
+    cy.contains('Kabul');
+
+    cy.get('a[href*="/country/CHN"]').click();
+
+    cy.url().should('include', '/country/CHN');
+
+    cy.contains('Beijing');
+  })
+})
